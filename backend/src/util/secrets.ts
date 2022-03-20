f2726381167e52027f8f07ea8f3eb6c8e000892c
amroczeK <mroczek9990@gmail.com>
2022-03-21 07:40:22 +0800

    secrets update

--- backend/src/util/secrets.ts
+++ backend/src/util/secrets.ts
@@ -1,7 +1,16 @@
-import path from "path";
 import logger from "./logger";
 import dotenv from "dotenv";
-dotenv.config({ path: path.join(__dirname, ".env") });
+import fs from "fs";
+
+if (fs.existsSync("src/.env")) {
+  logger.debug("Using .env file to supply config environment variables");
+  dotenv.config({ path: "src/.env" });
+} else {
+  logger.debug(
+    "Using .env.example file to supply config environment variables"
+  );
+  dotenv.config({ path: "src/.env.example" }); // you can delete this after you create your own .env file!
+}
 
 export const ENVIRONMENT = process.env.NODE_ENV;
 const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
